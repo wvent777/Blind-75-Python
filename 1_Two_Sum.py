1cb03dc4ad38f730a55f8cbdf5dca3c825fcf4d8
William Ventura <91572153+wvent777@users.noreply.github.com>
2022-12-12 22:29:45 -0500

    Update 1_Two_Sum.py

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,9 +1,8 @@
 class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    # Using a Hash Map
-    d = {} 
-    for i, num in enumerate(nums):
-      if target - num in d:
-        return d[target - num], i 
-      d[num] = i 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, num in enumerate(nums):
+            if target-num in d:
+                return d[target-num], i
+            d[num] = i
     
