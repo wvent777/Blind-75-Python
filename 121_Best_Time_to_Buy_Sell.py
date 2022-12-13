54d57ce0afdf10937ef1d8151a1abbfdf689c301
William Ventura <91572153+wvent777@users.noreply.github.com>
2022-12-12 22:27:20 -0500

    Create 121_Best_Time_to_Buy_Sell.py

---
+++ 121_Best_Time_to_Buy_Sell.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price, profit = math.inf, 0
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price-min_price)
+        return profit 
+        
